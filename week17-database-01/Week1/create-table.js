e35cff64c269595748d8eba8e235b6f0af41d1ae
Caglar Kaya <caglar.kaya.europe@gmail.com>
2022-01-13 21:23:53 +0100

    add week17

---
+++ week17-database-01/Week1/create-table.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'hyfuser',
+  password: 'hyfpassword',
+  database: 'company',
+});
+
+connection.connect();
+
+var create_query =
+  'create table projects (project_id int, project_name varchar(50), start_date date, manager varchar(50))';
+
+connection.query(create_query, function (error, results, fields) {
+  if (error) {
+    throw error;
+  }
+  console.log('the reply is ', results[0]);
+});
+connection.end();
