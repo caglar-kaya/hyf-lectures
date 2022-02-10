8ae65c2b165b885dcb0bcecff90becde38a804a5
Caglar Kaya <caglar.kaya.europe@gmail.com>
2022-02-10 16:27:34 +0100

    add week21 test

--- week21-react-02/api-calls/src/App.test.js
+++ week21-react-02/api-calls/src/App.test.js
@@ -1,8 +1,21 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('Loading...', () => {
+    render(<App />);
+    const loadingElement = screen.getByText(/Loading.../i);
+    expect(loadingElement).toBeInTheDocument();
+  });
+  test('quia et suscipit', () => {
+    render(<App />);
+    const loadingElement = screen.queryByText(/quia et suscipit/i);
+    expect(loadingElement).toBeNull();
+  });
+  test('exist architecto', async () => {
+    render(<App />);
+    expect(screen.queryByText(/architecto/i)).toBeNull();
+    expect(await screen.findByText(/architecto/i)).toBeInTheDocument();
+  });
 });
